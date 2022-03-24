64ca9970de9dae0ccc7293697e8f1f77364b6670
jackson273 <63116993+jackson273@users.noreply.github.com>
2022-03-25 01:13:04 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, redirect
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # return redirect(url_for("profile"))
+    return redirect(url_for("profile"))
+
+@app.route("/profile")
+def profile():
+    return render_template("index.html")
+
+@app.route("/static/<path:file_path>")
+def files(file_path):
+    return url_for('static', filename=file_path)
+
+if __name__ == "__main__":
+    app.run("localhost", 2703, debug=True)
---
+++ my_env/readme.md
@@ -0,0 +1,9 @@
+Home work 1
+===========
+A clean profile page
+
+* `/` - index page, redirect to `/profile`
+* `/profile` - profile page
+* `/static/<file_name>` - access to static files
+
+Github page is available at [dobaohoang-hw1.github.io](https://hoang97.github.io/dobaohoang-hw1.github.io/)
